ba30cc5d54f0ba809a85a4a1951576ed13cea53f
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-01-04 16:44:35 -0500

    refactor(app): function component to class syntax

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Migrating to React...
-        </p>
-        <a
-          className="App-link"
-          href="https://www.terminatevirus.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TerminateVirus
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Migrating to React...
+          </p>
+          <a
+            className="App-link"
+            href="https://www.terminatevirus.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            TerminateVirus
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
