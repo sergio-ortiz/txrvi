0d3f4cc88d8e363aeda1bb5f7c845cdf26f6075e
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-02-01 13:53:52 -0500

    feat(cloudinary): react sdk and corona image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,24 @@
 import React from "react";
 import Nav from "./Nav";
 import "./Header.css";
+import { AdvancedImage } from "@cloudinary/react";
+import { Cloudinary } from "@cloudinary/url-gen";
 
 function Header() {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: "terminatevirus",
+    },
+  });
+
+  const hero = cld.image("corona");
+  hero.format("auto");
+  hero.quality("auto");
+
   return (
     <header>
       <Nav />
-      <div className="spacer"></div>
+      <AdvancedImage cldImg={hero} />
       <h1 className="main-heading">
         Welcome to TerminateVirus.
         <br />
